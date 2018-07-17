5a5167ff6d1c99b7e72094bbf9c1ee8c5e600028
kevinjiao <kevin.jiao@youyoutingche.com>
2018-07-17 02:55:35 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title','Sample v1')</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+    <h1>主页君</h1>
+@endsection
